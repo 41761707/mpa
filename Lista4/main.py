5a2ecd1827389ad0e4e436414b9202f16e581f2d
unknown <wojtczakov@gmail.com>
2024-01-18 19:52:53 +0100

    Poprawka algorytmu

--- Lista4/main.py
+++ Lista4/main.py
@@ -3,12 +3,18 @@ import random
 import math
 import matplotlib.pyplot as plt
 
+f_distance_global = 0
+
 def generate_random_uniform_input(n):
     return [(random.uniform(0, 100), random.uniform(0, 100)) for _ in range(n)]
+    #return [(random.randint(0,101), random.randint(0,101)) for _ in range(n)]
 
 def f_distance(p1, p2):
+    global  f_distance_global 
+    f_distance_global= f_distance_global + 1
     #print(p1,p2)
     #print(math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2) )
+
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2) 
 
 def show_on_grid(points):
@@ -45,11 +51,11 @@ def brute_force_closest_pair(points):
 
     return closest_pair, min_distance
 
-def boundary_points(sorted_points, delta, best_pair):
+def boundary_points(points, sorted_points, delta, best_pair):
     #print("BOUNDARY: ", delta)
     n = len(sorted_points)
     mid_y = sorted_points[n//2][1]
-    to_consider = [point for point in sorted_points if mid_y - delta <= point[1] <= mid_y + delta]
+    to_consider = [point for point in points if mid_y - delta <= point[1] <= mid_y + delta]
     #print("TO CONSIDER: ", to_consider)
 
     best = delta
@@ -57,14 +63,16 @@ def boundary_points(sorted_points, delta, best_pair):
 
     for i in range(sub_n - 1):
         for j in range(i+1, min(i+7, sub_n)):
-            distance = f_distance(sorted_points[i],sorted_points[j])
+            distance = f_distance(to_consider[i],to_consider[j])
             if distance < best:
                 best = distance
-                best_pair = (sorted_points[i],sorted_points[j])
+                best_pair = (to_consider[i],to_consider[j])
+                #print("NEW MIN: ", distance)
+                #print("NEW MIN PAIR: ", best_pair)
     return best_pair, best
 
 
-def recursive(sorted_points):
+def recursive(points, sorted_points):
     #print("SORTED_POINTS: ", sorted_points)
     n = len(sorted_points)
     if n <= 3:
@@ -74,8 +82,8 @@ def recursive(sorted_points):
     #print("LEFT_HALF: ", left_half)
     right_half = sorted_points[mid:]
     #print("RIGHT_HALF: ", right_half)
-    left_closest_pair, left_distance = recursive(left_half)
-    right_closest_pair, right_distance = recursive(right_half)
+    left_closest_pair, left_distance = recursive(points, left_half)
+    right_closest_pair, right_distance = recursive(points, right_half)
 
     delta = 0
     current_best = 0
@@ -85,19 +93,18 @@ def recursive(sorted_points):
     else:
         delta = right_distance
         current_best = right_closest_pair
-    #print(delta)
-    #print(current_best)
-    closest_pair, min_distance = boundary_points(sorted_points, delta, current_best)
+    #print("DELTA: ", delta)
+    #print("CURRENT BEST: ",current_best)
+    closest_pair, min_distance = boundary_points(points, sorted_points, delta, current_best)
     return closest_pair, min_distance
 
 def solution(points):
     sorted_points = sorted(points, key = lambda x : x[1])
-    return recursive(sorted_points)
+    return recursive(sorted_points, sorted_points)
 
 def verify(points):
     closest_points = None
     min_distance = float('inf')
-
     for i in range(len(points) - 1):
         for j in range(i + 1, len(points)):
             distance = f_distance(points[i], points[j])
@@ -105,27 +112,31 @@ def verify(points):
             if distance < min_distance:
                 min_distance = distance
                 closest_points = (points[i], points[j])
-
     return closest_points, min_distance
 
 
 def test_loop(iters):
     for i in range(iters):
+        global f_distance_global 
+        f_distance_global = 0
         #print(generate_random_uniform_input(3))
         points = generate_random_uniform_input(20)
         #show_on_grid(points)
         #points = [(1,1), (5,1), (9,1), (13,1), (1,3), (5,3), (9,3), (13,3), (7,2)]
+        #points = [(25, 61), (74, 2), (75, 100), (43, 70), (5, 25), (91, 54), (38, 9), (21, 50), (31, 52), (96, 54), (93, 0), (45, 33), (30, 8), (69, 98), (67, 13), (70, 47), (59, 25), (47, 40), (78, 7), (85, 19)]
         #print(points)
         #print(solution(points))
         closest_pair_alg, distance_alg = solution(points)
-        closest_pair_force, distance_force = verify(points)
+        print("Liczba wywołań funkcji dystansu: ", f_distance_global)
+        #closest_pair_force, distance_force = verify(points)
+        closest_pair_force = None
+        distance_force = -1
         #print("Algorytm z zajec: {} -> {}".format(closest_pair_alg, distance_alg))
         #print("Algorytm brute-force: {} -> {}".format(closest_pair_force, distance_force))
         '''if distance_alg == distance_force:
             print("Algorytmy zwróciły taką samą odległość")
         else:
             print("Algorytmy zwróciły inne odległości")'''
-        
         if distance_alg != distance_force:
             show_on_grid_w_closest(points, closest_pair_alg)
             print("Algorytm z zajec: {} -> {}".format(closest_pair_alg, distance_alg))
